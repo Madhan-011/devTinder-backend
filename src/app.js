c6c7bf6abb9a8670ccdd60f8863955228a3017a6
madhan <madhandev005@gmail.com>
2026-09-15 17:42:54 +0530

    feat: implement user CRUD APIs with Express and Mongoose

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json()); // Here json is a middleware which is used to convert the incoming dynamic json data to js object and give it to the req.body
+
+
+//Create a user 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Anandhu",
-    lastName: "Aji",
-    email: "anandhu@gmail.com",
-    password: "987654",
-  });
+  // console.log(req.body)
+
+  const user = new User(req.body);
   try {
     await user.save();
     res.send(user);
@@ -18,6 +19,52 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error while saving user: " + error.message);
   }
 });
+
+//Get Api for one
+app.get("/user", async (req, res) => { //findOne API
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if(!user){
+      res.status(404).send("User not found")
+    }else{
+    res.send(user);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Get Api for all 
+app.get("/feed",async (req,res) => {
+  try{
+  const user = await User.find()
+  res.send(user)
+  }catch(err){
+    res.status(400).send("Something went wrong",err)
+  }
+})
+
+//Delete Api for user
+app.delete("/user", async (req,res) => {
+  // const userDeleted1 = await User.findByIdAndDelete({id: req.body.id}) // this is equal to only req.body.id below
+  try{
+  const userDeleted = await User.findByIdAndDelete(req.body.id)
+  res.send("User Deleted successfully")
+  }catch(err){
+    res.status(400).send("Something went wrong")
+  }
+})
+
+//Update Api using patch
+app.patch("/user",async (req,res) => {
+  try{
+  const user = await User.findByIdAndUpdate(req.body.userId, req.body)
+  res.send("User Updated successfully")
+  }catch(err){
+    res.status(400).send("Something went wrong",err)
+  }
+})
+
 
 connectDB()
   .then(() => {
