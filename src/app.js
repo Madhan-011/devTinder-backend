c7a6e911c926321c9464bb9431c7415bcbe0e259
madhan <madhandev005@gmail.com>
2026-09-16 02:03:59 +0530

    feat: add user login API with password verification

--- src/app.js
+++ src/app.js
@@ -30,11 +30,33 @@ app.post("/signup", async (req, res) => {
     });
 
     await user.save();
-    res.send(user);
+    res.send("Created User Successfully");
   } catch (error) {
     res.status(400).send("Error: " + error.message);
   }
 });
+
+//Login Api
+app.post("/login", async (req,res) => {
+  try{
+    const {email,password} = req.body
+    
+    const user = await User.findOne({email:email})
+    if(!user){
+      throw new Error("Invalid Credentials")
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(isPasswordValid){
+      res.send("Login Successful...")
+    }else{
+      throw new Error("Invalid Credentials")
+    }
+  }catch(err){
+    res.status(400).send("Error : " + err.message);
+  }
+})
 
 //Get Api for one
 app.get("/user", async (req, res) => {
