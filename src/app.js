382dc6f473da99493eecf2d111996c7278a2aece
madhan <madhandev005@gmail.com>
2026-09-22 16:34:06 +0530

    feat: add API to fetch received connection requests

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
-const authRouter = require("./routers/auth");
-const profileRouter = require("./routers/profile");
-const requestRouter = require("./routers/request");
 
 const app = express();
 
 app.use(cookieParser()); // It is mainly for parsing/reading cookies
 app.use(express.json()); // Here json is a middleware which is used to convert the incoming dynamic json data to js object and give it to the req.body
 
+
+const authRouter = require("./routers/auth");
+const profileRouter = require("./routers/profile");
+const requestRouter = require("./routers/request");
+const userRouter = require("./routers/user");
+
+
 app.use("/", authRouter)
 
 app.use("/", profileRouter)
 
 app.use("/", requestRouter)
+
+app.use("/", userRouter)
 
 
 connectDB()
---
+++ src/routers/user.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const userRouter = express.Router();
+
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequestModel = require("../models/connctionRequest");
+
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequestModel.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate(
+      "fromUserId",
+      "firstName lastName photoUrl age gender about skills",
+    );
+
+    res.json({
+      message: "Data fetched successfully",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+module.exports = userRouter;
